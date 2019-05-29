a60f83272761f54fe57c490caf9cf0ddf24b8337
IYefremov <iyefremov@cyberiansoft.com>
2019-05-29 16:48:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+//   Write a program which prints to the console some string 5 times.
+//   1) New string variable should be created.
+//   2) Each string should be printed from a new line."
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String string1 = "My first string ";
+        String string2 = "My second string";
+        String string3 = "My third string";
+        String string4 = "My fourth string";
+        String string5 = "My fifth string";
+
+
+
+        System.out.println(string1);
+        System.out.println(string2);
+        System.out.println(string3);
+        System.out.println(string4);
+        System.out.println(string5);
+    }
+}
